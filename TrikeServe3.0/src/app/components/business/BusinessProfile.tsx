209823d425267f83c1a80f21cb8bffe5019a9712
mayoi <robivancoleco@plv.edu.ph>
2026-08-24 01:30:57 +0800

    cleanup1

---
+++ TrikeServe3.0/src/app/components/business/BusinessProfile.tsx
@@ -0,0 +1,8 @@
+export default function BusinessProfile() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Business Profile</h1>
+      <p>Profile page coming soon.</p>
+    </div>
+  );
+}
